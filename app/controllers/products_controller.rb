64089d149d8e5e1f3d3b9c7dc70f447ae9710a19
Kelsey Fecho <kfecho30@gmail.com>
2018-07-02 01:21:39 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,4 +16,15 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    Product.create(product_params)
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :inventory, :price)
+  end
 end
