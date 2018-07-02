e896ac4e7069c579f445b3835946d435a54d7264
Kelsey Fecho <kfecho30@gmail.com>
2018-07-02 01:22:23 +0000

    automatically backed up by learn

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,12 +3,4 @@ class Product < ActiveRecord::Base
   has_many :orders, :through => :ordered_products
   attr_accessor :description, :inventory
 
-  def description
-    return "description goes here"
-  end
-
-
-  def inventory
-    self.all.count
-  end
 end
