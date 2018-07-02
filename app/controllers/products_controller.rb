5f764f78c5f03c7469a1ff4adf0a691a8b1abf37
Kelsey Fecho <kfecho30@gmail.com>
2018-07-02 01:12:33 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,8 @@ class ProductsController < ApplicationController
   end
 
   def body
-    post = Post.find(params[:id])
-    render plain: post.description
+    product = Product.find(params[:id])
+    render plain: product.description
   end
 
   def new
